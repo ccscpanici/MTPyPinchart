2787a41926fcc200163ae058d5326d23b250edfd
nunchuckBoP <41088356+nunchuckBoP@users.noreply.github.com>
2020-09-14 14:23:23 -0500

    working

--- worker.py
+++ worker.py
@@ -36,8 +36,17 @@ def process_sheet(elock, opclock, slock, pc5lock, main_sheet_object, \
     # gets the PLC data schema
     _plc_data_structure = sheet_object.get_plc_data_structure()
 
+    # sets a volitile bit - this is used to exit the loop
+    # on an error
+    running = True
+
     # loop through different chunks of data
     for plc_data_column in _plc_data_structure:
+
+        if not running:
+            utils.output(thread_id, "worker", "process_sheet", "TERMINATED UNEXPECTIDLY", slock)
+            break
+        # end if
 
         if operation == PLC_OPERATION_UPLOAD or operation == PLC_OPERATION_DOWNLOAD:
 
@@ -50,6 +59,8 @@ def process_sheet(elock, opclock, slock, pc5lock, main_sheet_object, \
             # get the plc data for the column
             plc_data_column['plc_data'] = sheet_object.get_plc_data_for_column(plc_data_column)
 
+            # waits until no other thread is using
+            # the opc object.
             opclock.acquire()
             opc = OpenOPC.client()
             opc.connect(OPC_SERVER)
@@ -73,7 +84,14 @@ def process_sheet(elock, opclock, slock, pc5lock, main_sheet_object, \
             data_tuples = sheet_object.get_address_value_list(plc_data_column['plc_data'], plc_data_column['data']['type'])
 
             utils.output(thread_id, "worker", "process_sheet", "%s-GETTING DOWNLOADING DATA CHUNK..." % sheet_name, slock)
-            _return_data = opc.write(data_tuples)
+
+            try:
+                _return_data = opc.write(data_tuples)
+            except Exception as ex:
+                utils.output(thread_id, "worker", "process_sheet", "UNHANDLED ERROR: %s" % ex, slock)
+                running = False
+                _return_data = None
+            # end try
 
             opclock.release()
 
@@ -81,39 +99,50 @@ def process_sheet(elock, opclock, slock, pc5lock, main_sheet_object, \
             # on the download - we probably don't have to, maybe print
             # any errors?
 
+            if _return_data:
+                # process the data from the opc function. Maybe raise an error,
+                # if too many errors were returned?
+                pass
+            # end _return_data
+
         elif operation == PLC_OPERATION_UPLOAD:
             
             # gets the address list for upload
             utils.output(thread_id, "worker", "process_sheet", "%s-GETTING PLC ADDRESSES..." % sheet_name, slock)
             addresses = sheet_object.get_address_list(plc_data_column['plc_data'])
 
+            utils.output(thread_id, "worker", "process_sheet", "%s-GETTING UPLOADING DATA CHUNK..." % sheet_name, slock)
+            
             try:
-                utils.output(thread_id, "worker", "process_sheet", "%s-GETTING UPLOADING DATA CHUNK..." % sheet_name, slock)
                 _return_data = opc.read(addresses)
-            except OpenOPC.TimeoutError as ex:
-                utils.output(thread_id, "worker", "process_sheet", "TIMEOUT ERROR: %s" % ex, slock)
+            except Exception as ex:
+                _return_data = None
+                utils.output(thread_id, "worker", "process_sheet", "UNHANDLED ERROR: %s" % ex, slock)
+                running = False
             # end try
             
             opclock.release()
             
-            # process the return data, if there are a bunch
-            # of errors, maybe kick it out?
-            plc_data_column['plc_data'] = sheet_object.update_data_with_new_values(plc_data_column['data']['type'], plc_data_column['plc_data'], _return_data)
+            if _return_data:
+                # process the return data, if there are a bunch
+                # of errors, maybe kick it out?
+                plc_data_column['plc_data'] = sheet_object.update_data_with_new_values(plc_data_column['data']['type'], plc_data_column['plc_data'], _return_data)
 
-            # lock the execl
-            elock.acquire()
+                # lock the execl
+                elock.acquire()
 
-            # create the win32com excel interface class
-            utils.output(thread_id, "worker", "process_sheet", "%s-GETTING WORKBOOK..." % sheet_name, slock)
-            _excel = excel_interface.Interface(full_file_path, sheet_name)
+                # create the win32com excel interface class
+                utils.output(thread_id, "worker", "process_sheet", "%s-GETTING WORKBOOK..." % sheet_name, slock)
+                _excel = excel_interface.Interface(full_file_path, sheet_name)
 
-            # hand the interface class the data that needs 
-            # to be updated
-            utils.output(thread_id, "worker", "process_sheet", "%s-UPDATING WORKSHEET WITH DATA CHUNK..." % sheet_name, slock)
-            _excel.update_sheet(plc_data_column, config_data)
+                # hand the interface class the data that needs 
+                # to be updated
+                utils.output(thread_id, "worker", "process_sheet", "%s-UPDATING WORKSHEET WITH DATA CHUNK..." % sheet_name, slock)
+                _excel.update_sheet(plc_data_column, config_data)
 
-            # after it is all updated, release the lock
-            elock.release()
+                # after it is all updated, release the lock
+                elock.release()
+            # end if
 
         elif operation == PLC_OPERATION_IMPORT:
 
