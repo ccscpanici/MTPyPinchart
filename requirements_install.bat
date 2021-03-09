c32091152f9bffcaea6d7daf6de80cc14f729896
nunchuckbop <cj@neechsoft.com>
2021-03-09 13:43:55 -0600

    changed numpy version

--- requirements_install.bat
+++ requirements_install.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\Python39\Scripts\pip.exe" install numpy-1.19.4-cp39-cp39-win_amd64.whl
+"C:\Program Files\Python39\Scripts\pip.exe" install numpy-1.20.1-cp39-cp39-win_amd64.whl
 "C:\Program Files\Python39\Scripts\pip.exe" install -r requirements.txt
